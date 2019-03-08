5aaca0dadf96b8db673fdecceca2ec270637af43
lord_santanna <adriacabeza@hotmail.com>
2019-03-08 22:33:54 +0100

    Basic structure for Flask

---
+++ src/__main__.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, redirect, request
+
+flask_app = Flask(__name__, template_folder='templates/')
+flask_app.config['JSON_AS_ASCII'] = False
+
+@flask_app.route('/')
+def index():
+#  return render_template('index.html',params=None)
+	return "<h1>Home Page</h1>"
+
+if __name__ == '__main__':
+	flask_app.run(host='0.0.0.0', port=8081, debug =True)
+
+
+#if __name__ == '__init__':
+#ficar initial stuff
