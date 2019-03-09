757518f7a6da5aca6f1a725ccc8bfaafcc06d60f
adriacabeza <adriacabeza@hotmail.com>
2019-03-09 09:20:13 +0100

    environment and requirements

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -19,13 +19,16 @@ def optimizeImage(ImageName, option='faceLogin/Faces'):
 
 #OCR STUFF
 
-keywords = ['Permis','conduire','Licenza','condurre','Fuhrerausweis','Driving', 'License','Permiss','manischar','Fohreraumwets-Perwes','Fuhrerausweis-Permis','Licence','Permiss','roanischar']
+keywords = ['Permis','conduire','Licenza','condurre','Fiihrerausweis','Permisdeconduire',
+'Licenzadicondurre','Permissda','conesdicondurts','Fuhrerausweis','Driving', 'License','Permiss',
+'manischar','Fohreraumwets-Perwes','Fuhrerausweis-Permis','Licence','Permiss','roanischar']
 #OCR is not that good in all kinds of images so I have put some of the inputs that I get with real values
 
 def check_if_driver_license(ImageName):
   image = cv2.imread(r'../DriverLicense/'+ImageName)
   text = pytesseract.image_to_string(Image.fromarray(image))
   text = text.replace(" ","")
+  print(text)
   if len(text) > 0 and any(x in text for x in keywords):
     print('IT IS A FUCKING DRIVING LICENSE BRO I GUESS')
     return True
@@ -40,6 +43,11 @@ def tests():
   check_if_driver_license('license2.jpg')
   check_if_driver_license('license5.jpg')
   check_if_driver_license('license3.jpg')
-  check_if_driver_license('notalicense.jpg')
-  check_if_driver_license('notalicense2.jpg')
+ # check_if_driver_license('notalicense.jpg')
+  check_if_driver_license('license5.jpg')
+  check_if_driver_license('license6.jpg')
+ # check_if_driver_license('notalicense2.jpg')
   check_if_driver_license('prueba.jpg')
+
+
+tests()
