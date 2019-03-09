9bcf182193a2c385b2560ce4a91ba1ee7a536b3f
adriacabeza <adriacabeza@hotmail.com>
2019-03-09 02:12:19 +0100

    faceLogin done

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, request
-from src.faceLogin import faceLogin
+from faceLogin import faceLogin
 
 
 flask_app = Flask(__name__, template_folder='templates/')
@@ -7,11 +7,13 @@ flask_app.config['JSON_AS_ASCII'] = False
 
 @flask_app.route('/')
 def index():
-#  return render_template('index.html',params=None)
+	faceLogin.detectUser()
+#return render_template('index.html',params=None)
 	return "<h1>Home Page</h1>"
 
 if __name__ == '__main__':
 	flask_app.run(host='0.0.0.0', port=8081, debug =True)
+	
 
 
 #if __name__ == '__init__':
