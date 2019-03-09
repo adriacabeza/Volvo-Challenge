ce4aa4dbe13e79258535a6d93308844663f1bc3e
laca <xlcbdn@gmail.com>
2019-03-09 20:27:09 +0100

    LOGIN WORKING BABY

--- app/app.py
+++ app/app.py
@@ -1,22 +1,27 @@
-from flask import Flask, render_template, redirect, request
 from pymongo import MongoClient
 from pymongo.errors import DuplicateKeyError
 import os
 from os.path import join, dirname, realpath
 import faceLoginLogin
 from werkzeug.security import generate_password_hash
-import pprint
+from models.user import User
+from flask import Flask, Response, redirect, url_for, request, session, abort, render_template
+from flask_login import LoginManager, UserMixin, \
+login_required, login_user, logout_user 
 
 
 mongo_uri = os.environ.get("MONGODB_URI", "mongodb://heroku_xv3vfwld:l3f3d2fv550d1akktp8m9uqj8e@ds119380.mlab.com:19380/heroku_xv3vfwld")
 
 client = MongoClient(mongo_uri)
 db = client['heroku_xv3vfwld']
-collection = db['provaVOLVO']
 
 app = Flask(__name__, template_folder='templates/')
 app.config['JSON_AS_ASCII'] = False
-
+app.config['SESSION_TYPE'] = 'memcached'
+app.config['SECRET_KEY'] = 'STARTHACKbaby'
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
 
 @app.route('/')
 def index():
@@ -26,6 +31,18 @@ def index():
 def login():
 	return render_template('login.html',params=None)
 
+@lm.user_loader
+def load_user(username):
+	u = db['users'].find_one({"_id": username})
+	if not u:
+		return None
+	return User(u['_id'])
+
+@app.route('/write', methods=['GET', 'POST'])
+@login_required
+def write():
+	return "<h1>HELLO</h1>"
+
 @app.route('/signup', methods = ['GET', 'POST'])
 def upload_file():
 	if request.method == 'GET':
@@ -33,11 +50,18 @@ def upload_file():
 	if request.method == 'POST':
 		selfie = request.files['selfie']
 		licen = request.files['license']
-		pprint(request)
+		name = request.form['name']
+		email = request.form['email']
+		password = request.form['password']
+		pass_hash = generate_password_hash(password, method='pbkdf2:sha256')
 		if faceLoginLogin.registerUser(selfie, licen):
 			try:
-				collection.insert({"_id": user, "password": pass_hash})
+				db['users'].insert({"_id": name, "password": pass_hash, "email": email})
 				print ("User created.")
+				user = db['users'].find_one({"_id": name})
+				login_user(User(name))
+				flash("Logged in successfully", category='success')
+				return redirect("/")
 			except DuplicateKeyError:
 				print ("User already present in DB.")
 		else:
