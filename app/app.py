635c5533accd4419fbfaa1ef6b716f15327322c3
laca <xlcbdn@gmail.com>
2019-03-10 12:25:17 +0100

    deploy

--- app/app.py
+++ app/app.py
@@ -70,7 +70,10 @@ def home():
 @login_required
 def dashboard():
 	if current_user.owner:
-		return render_template('/Dashboard.html', crashes = list(db['crashes'].find()))
+		c = list(db['crashes'].find())
+		for cr in c:
+			cr['_id'] = int(cr['_id'])
+		return render_template('/Dashboard.html', crashes = c)
 	else:
 		return redirect('/home')
 
@@ -112,7 +115,7 @@ def upload_file():
 
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 8081))
-	app.run(host='localhost', port=port, debug =True)
+	app.run(host='0.0.0.0', port=port, debug =True)
 	
 	
 	
