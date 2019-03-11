7c5b0e2c82b791b416550568d299a304011f8032
adriacabeza <adriacabeza@hotmail.com>
2019-03-11 21:39:17 +0100

    ara si que es una webapp

---
+++ app/static/js/sw.js
@@ -0,0 +1,38 @@
+var CACHE_NAME = 'v1';
+
+var urlsToCache = [
+  //'.',
+  '/static/splash_offline.html',
+  '/static/css/splash.css',
+  '/static/images/android-chrome-192x192.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => {
+          return cacheName != CACHE_NAME;
+        }).map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
